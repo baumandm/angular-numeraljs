f8b17276785e24cf0aeb7ed14b44ca59d26205ef
Dave Bauman <baumandm@gmail.com>
2014-08-21 10:27:29 -0700

    Improve the bower config and bump to 1.0.0

--- dist/angular-numeraljs.min.js
+++ dist/angular-numeraljs.min.js
@@ -1,7 +1,7 @@
 /**
  * AngularJS filter for Numeral.js: number formatting as a filter
- * @version v0.1.0 - 2013-11-09
- * @link 
+ * @version v1.0.0 - 2014-08-21
+ * @link https://github.com/baumandm/angular-numeraljs
  * @author Dave Bauman <baumandm@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
