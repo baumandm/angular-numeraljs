a1c15e3842a8336d4a69432993f771bad9e299a1
Marc Morales Valldepérez <marcmorales83@gmail.com>
2014-09-05 16:19:55 +0200

    added language option

--- dist/angular-numeraljs.js
+++ dist/angular-numeraljs.js
@@ -9,11 +9,13 @@
 'use strict';
 angular.module('ngNumeraljs', [])
     .filter('numeraljs', function () {
-        return function (input, format) {
+        return function (input, format, language) {
             if (input == null || format == null) 
                 return input;
             if (format === '') 
                 return '';
+            if(language != null)
+                numeral.language(language);
             
             return numeral(input).format(format);
         };
