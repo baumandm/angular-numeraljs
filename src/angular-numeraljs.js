ed6c2190d51fb08e9eabec3fe369dc460f170d21
Dave Bauman <baumandm@gmail.com>
2014-09-16 09:05:20 -0700

    Add language option to the src files, regenerate dist files.  Bump version

--- src/angular-numeraljs.js
+++ src/angular-numeraljs.js
@@ -2,11 +2,14 @@
 'use strict';
 angular.module('ngNumeraljs', [])
     .filter('numeraljs', function () {
-        return function (input, format) {
+        return function (input, format, language) {
             if (input == null || format == null) 
                 return input;
             if (format === '') 
                 return '';
+            if (language != null)
+                numeral.language(language);
+
             
             return numeral(input).format(format);
         };
