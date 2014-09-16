2ffdb87552923cd91582cd2409d73acd7764264f
Dave Bauman <baumandm@gmail.com>
2014-09-16 10:29:00 -0700

    Rebuild dist/min versions, bump to 1.0.3

--- dist/angular-numeraljs.js
+++ dist/angular-numeraljs.js
@@ -1,23 +1,24 @@
 /**
  * AngularJS filter for Numeral.js: number formatting as a filter
- * @version v1.0.2 - 2014-09-16
+ * @version v1.0.3 - 2014-09-16
  * @link https://github.com/baumandm/angular-numeraljs
  * @author Dave Bauman <baumandm@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
 /*global numeral */
 'use strict';
+
 angular.module('ngNumeraljs', [])
     .filter('numeraljs', function () {
         return function (input, format, language) {
-            if (input == null || format == null) 
+            if (!input || !format) {
                 return input;
-            if (format === '') 
-                return '';
-            if (language != null)
-                numeral.language(language);
+            }
 
-            
+            if (language) {
+                numeral.language(language);
+            }
+
             return numeral(input).format(format);
         };
     });
