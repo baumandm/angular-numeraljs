a2b5e17755bb38b4948aee06c132ee81a4957e31
Dave Bauman <baumandm@gmail.com>
2014-09-21 12:24:45 -0700

    Rework examples to use ngRepeat and display in a table

--- example/config/js/app.js
+++ example/config/js/app.js
@@ -1,33 +1,52 @@
 var app = angular.module('exampleApp', ['ngNumeraljs']);
 
 app.config(['$numeraljsConfigProvider', function ($numeraljsConfigProvider) {
-	var language = {
-		delimiters: {
-			thousands: ' ',
-			decimal: ','
-		},
-		abbreviations: {
-			thousand: 'k',
-			million: 'm',
-			billion: 'b',
-			trillion: 't'
-		},
-		ordinal: function (number) {
-			return '.';
-		},
-		currency: {
-			symbol: '€'
-		}
-	};
+    var language = {
+        delimiters: {
+            thousands: ' ',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            return '.';
+        },
+        currency: {
+            symbol: '€'
+        }
+    };
 
-	$numeraljsConfigProvider.setFormat('currency', '$ 0,0.00');
-	$numeraljsConfigProvider.setFormat('currencySuffix', '0,0.00 $');
-	$numeraljsConfigProvider.setFormat('number', '0.00');
-	$numeraljsConfigProvider.setLanguage('de', language);
+    $numeraljsConfigProvider.setDefaultFormat('0,0.00');
 
-	$numeraljsConfigProvider.setCurrentLanguage('de');
+    $numeraljsConfigProvider.setFormat('currency', '$ 0,0.00');
+    $numeraljsConfigProvider.setFormat('currencySuffix', '0,0.00 $');
+    $numeraljsConfigProvider.setFormat('number', '0.00');
+    $numeraljsConfigProvider.setLanguage('de', language);
+
+    $numeraljsConfigProvider.setCurrentLanguage('de');
 }]);
 
 app.controller('numeralExample', function ($scope) {
-
+    $scope.formats = [{
+        name: 'Default Format',
+    }, {
+        name: 'Number',
+        format: '0,0'
+    }, {
+        name: 'Currency',
+        format: '$0,0.00'
+    },{
+        name: 'Bytes',
+        format: '0b'
+    }, {
+        name: 'Percentages',
+        format: '0.0%'
+    }, {
+        name: 'Time',
+        format: '00:00:00'
+    }];
 });
